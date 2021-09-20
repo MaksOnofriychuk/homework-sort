19876540c5b353c973722c967fc680dbb7d91bc1
Онофрийчук Максим <onofrijcukmaksim@MacBook-Pro-Onofrijcuk.local>
2021-09-20 11:54:41 +0300

    Initial commit

---
+++ src/app/components/tableHeader.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const TableHeader = ({ onSort, selectedSort, columns }) => {
+   const handleSort = (item) => {
+      if (selectedSort.path === item) {
+         onSort((selectedSort) => ({ ...selectedSort, order: selectedSort.order === "asc" ? "desc" : "asc" }));
+      } else {
+         onSort({ path: item, order: "asc" });
+      }
+   };
+   const handleSortArrow = (selectedSort, currentPath) => {
+      if (selectedSort.path === currentPath) {
+         if (selectedSort.order === "asc") {
+            return <i className="bi bi-caret-down-fill"></i>;
+         } else {
+            return <i className="bi bi-caret-up-fill"></i>;
+         }
+      }
+      return null;
+   };
+   return (
+      <thead>
+                            <tr>
+                               {Object.keys(columns).map((column) => <th key={column} onClick={ columns[column].path ? () => handleSort(columns[column].path) : undefined } {...{ role: columns[column].path && "button" }} scope="col">{columns[column].name}{columns[column].path ? handleSortArrow(selectedSort, columns[column].path) : null}</th>)}
+                                {/* <th onClick={() => handleSort("name")} scope="col">Имя</th>
+                                <th scope="col">Качества</th>
+                                <th onClick={() => handleSort("profession.name")} scope="col">Провфессия</th>
+                                <th onClick={() => handleSort("completedMeetings")} scope="col">Встретился, раз</th>
+                                <th onClick={() => handleSort("rate")} scope="col">Оценка</th>
+                                <th onClick={() => handleSort("bookmark")} scope="col">Избранное</th>
+                                <th /> */}
+                            </tr>
+                        </thead>
+   );
+};
+TableHeader.propTypes = {
+   onSort: PropTypes.func.isRequired,
+   selectedSort: PropTypes.object.isRequired,
+   columns: PropTypes.object.isRequired
+};
+export default TableHeader;
